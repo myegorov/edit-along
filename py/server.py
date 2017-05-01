91e4b0d31b44829c2277c4b9c72ec303448c1dcf
Maksim Yegorov <findmaksim@gmail.com>
2017-04-30 23:35:10 -0400

    todo: implement JS client

--- py/server.py
+++ py/server.py
@@ -1,18 +1,49 @@
-from bottle import route, run, get, static_file
+from bottle import Bottle, route, run, get, static_file, request, abort
 
+from gevent import monkey; monkey.patch_all()
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+from geventwebsocket import WebSocketError
+
+from time import sleep #TODO: for testing only
+
+host = '127.0.0.1'
+port = 8080
+# app = Bottle()
+
+# @app.route('/')
 @route('/')
 def index():
     return static_file('index.html', root="../")
 
+# @app.get("/static/css/<filepath:re:.*\.css>")
 @get("/static/css/<filepath:re:.*\.css>")
 def css(filepath):
-    print("hello CSS")
     return static_file(filepath, root="../static/css")
 
+# @app.get("/static/js/<filepath:re:.*\.js>")
 @get("/static/js/<filepath:re:.*\.js>")
 def js(filepath):
-    print("hello JS")
     return static_file(filepath, root="../static/js")
 
+# @app.route('/websocket')
+@route('/websocket')
+def socks():
+    wsock = request.environ.get('wsgi.websocket')
+    if not wsock:
+        abort(400, 'Expected websocket request...')
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send('Your message was: %r' % message)
+        except WebSocketError:
+            break
 
-run(host='localhost', port=8080, debug=True)
+
+run(host=host, port=port, reloader=True, server='gevent', 
+        debug=True, handler_class=WebSocketHandler)
+
+# server = WSGIServer((host, port), app,
+#                     handler_class=WebSocketHandler)
+# print("access @ http://%s:%s/" %(host, port))
+# server.serve_forever()
--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,84 @@
 window.onload = function() {
+
+  // test websockets
+  let sock = new WebSocket("ws://localhost:8080/websocket");
+
   let textarea = document.getElementById('textarea');
   textarea.focus();
 
+
+
+  // test working with cursors
+  function getCaretCharacterOffsetWithin(element) {
+    var caretOffset = 0;
+    if (typeof window.getSelection != "undefined") {
+      var range = window.getSelection().getRangeAt(0);
+      var preCaretRange = range.cloneRange();
+      preCaretRange.selectNodeContents(element);
+      preCaretRange.setEnd(range.endContainer, range.endOffset);
+      caretOffset = preCaretRange.toString().length;
+    }
+    return caretOffset;
+  };
+
+  function setCaretCharacterOffsetWithin(element, ix) {
+    element.focus();
+    var range = document.createRange();
+    var sel = window.getSelection();
+    var relIx = 0;
+    var nodeIx = 0;
+    var found = false;
+    var ln = 0;
+    // alert(element.innerHTML);
+    for (var i = 0; i < element.childNodes.length; i++) {
+      // alert(element.childNodes[i].nodeType);
+      if (element.childNodes[i].nodeType == 3) {
+        ln = element.childNodes[i].length;
+      } else {
+        ln = element.childNodes[i].innerText.length;
+      }
+      if (ix >= ln) {
+        ix -= ln;
+      } else {
+        nodeIx = i;
+        relIx = ix;
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      alert("found loc at node " + nodeIx + " relIx " + relIx);
+      if (element.childNodes[nodeIx].nodeType == 3) {
+        range.setStart(element.childNodes[nodeIx], relIx);
+        range.setEnd(element.childNodes[nodeIx], relIx+1);
+      } else {
+        range.setStart(element.childNodes[nodeIx].firstChild, relIx);
+        range.setEnd(element.childNodes[nodeIx].firstChild, relIx+1);
+      }
+      range.collapse(true);
+      sel.removeAllRanges();
+      sel.addRange(range);
+    }
+  };
+
+
+  // test firing events on changes
   let observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-      // let rec = {
-      //   mutation: mutation,
-      //   el: mutation.target,
-      //   val: mutation.target.textContent,
-      //   oldVal: mutation.oldValue
-      // };
-      // console.log(rec);
+      sock.send(textarea.innerText);
 
-      console.log(textarea.innerText);
+      sock.onmessage = function(evt) {
+        console.log(evt.data);
+
+        console.log("selection (cursor??): " + 
+                    JSON.stringify(getCaretCharacterOffsetWithin(textarea)));
+
+        // now try resetting cursor to some arbitrary number
+        if (getCaretCharacterOffsetWithin(textarea) > 20) {
+          setCaretCharacterOffsetWithin(textarea, 8);
+        }
+      };
+
     });
   });
 
@@ -22,4 +88,6 @@ window.onload = function() {
   };
 
   observer.observe(textarea, observerOptions);
+
+
 }

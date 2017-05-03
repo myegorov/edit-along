f7de47a43db0f7ce6aa6648b0d8a6bf9087a6812
Maksim Yegorov <findmaksim@gmail.com>
2017-05-03 16:56:19 -0400

    continue with normal ops, i.e. patch()

--- py/ds.py
+++ py/ds.py
@@ -1,18 +1,37 @@
 """
 Differential synchronization logic for server.
 """
+# TODO: edits actually may be a stack (array of string patches)!!!
+# TODO: decide on whether to pass around diffs or patches?? diffs seems
+# safer on average, but patches more economical?? Currently shooting for
+# sending patches over network.
+#   'edits': [[0, 'Mac'], [1, 'intoshe'], \
+#             [0, 's had the original point and click '], \
+#             [-1, 'UI'], [1, 'interface'], [0, '.']],
+# or
+#   'edits': ["""@@ -1,11 +1,18 @@
+              #  Mac
+              # +intoshe
+              # s had th
+              # @@ -42,7 +42,14 @@
+              # ick
+              # -UI
+              # +interface
+              # ."""]
+
+
 
 from geventwebsocket import WebSocketError
 import diff_match_patch as dmp
 import json
 
 # TODO: need to synchronize on this one?
-CLIENT_FRESH = 0
-
-# TODO: need to synchronize on this one?
 # {key=client_id <int>, val={server_shadow <{str,[int,int]}>, backup_shadow <{str, int}>}}
 CLIENT_REC = {}
-SERVER_TEXT = ""
+# {key=doc_id<int>, val=server_text<str>}
+SERVER_TEXT = {}
+
+diffy = dmp.diff_match_patch()
 
 def conveyor_belt(consumer):
     """ An entry point coroutine that adds socket requests to 
@@ -79,16 +98,76 @@ def consumer():
     except StopIteration:
         return None
 
-# TODO: onboard new client
 def onboard():
-    pass
+    """Onboard new client, then on to normal operations."""
+    try:
+        wsock, url, message = (yield)
+
+        # get fresh client id (a hash on doc-client pair)
+        doc_id = url.rstrip('/').split('/')[-1]
+        ip = wsock.environ['REMOTE_ADDR']
+        client_id = hash(doc_id+ip)
+
+        # check if server_text exists for given doc_id
+        # do the minimal setup, then on to normal operation
+        # get fresh server text if it doesn't exist already
+        server_text = SERVER_TEXT.setdefault(doc_id, '')
+
+        clock = message.get('clock', None) # consistency check
+        assert clock == [-1,-1], \
+            "onboard() found inconsistent clock on handshake: %s" %message
+        server_shadow = {'text':server_text, 'clock': clock[:]} # initially [-1,-1]
+        backup_shadow = {'text':server_text, 'server_clock':clock[1]}
+        CLIENT_REC[client_id] = {'server_shadow':server_shadow, 
+                                'backup_shadow':backup_shadow}
+        # should be OK if edits are empty
+        edits = message.get('edits', ['']) # edits are a list of patch strings
+
+        # move on now
+        normal_ops = patch()
+        next(normal_ops)
+        normal_ops.send((wsock, url, {'clock':clock, 
+                                    'edits':edits,
+                                    'client_id':client_id}))
+
+    except WebSocketError:
+        wsock.close()
+    except GeneratorExit:
+        return None
+    except StopIteration:
+        return None
+
 
 # TODO: normal operation (steps 5a onwards)
 def patch():
+
+    # # {key=client_id <int>, val={server_shadow <{str,[int,int]}>, backup_shadow <{str, int}>}}
+    # CLIENT_REC = {}
+
+    # # {key=doc_id<int>, val=server_text<str>}
+    # SERVER_TEXT = {}
+
+    # {
+    #   'clock':[<CLIENT_CLOCK>, <SERVER_CLOCK>], # negative on first handshake
+    #   'edits': "@@ -1,11 +1,18 @@\n Mac\n+intoshe\n s had th\n@@ -42,7 +42,14 @@\n ick \n-UI\n+interface\n .\n",
+    #   'client_id': int # None on first handshake, hash of client-doc
+    # }
+
+
+
+    wsock, url, message = (yield)
+
+
+    # edits = message.get('edits', None)
+    # if edits is not None:
+    #     server_text = diffy.patch_apply(diffy.patch_fromText(edits), '')
+
+
     pass
 
 # TODO: address one of error scenarios
 def manage_failure():
+    wsock, url, message = (yield)
     pass
 
 
--- py/tests/diff_tests.py
+++ py/tests/diff_tests.py
@@ -14,7 +14,8 @@ oo = 'Macs had the original point and click UI.'
 nc = 'Macintoshes had the original point and click interface.'
 ns = 'Smith & Wesson had the original point and click UI.'
 
-# delta is the currency of exchange between client and server
+# delta is the currency of exchange between client and server?
+# or use patches?
 delta = diff.diff_main(oo, nc)
 print('delta:', delta)
 # JSON encode
@@ -24,6 +25,29 @@ print('JSON formatted delta:', delta_json)
 delta_ = json.loads(delta_json)
 print('delta decoded from JSON:', delta_)
 
+# over network best to transfer patch, not diff
+# calculate patches to turn original text via diff into new text (Method 3)
+# returns array of patch objects
+# note that dmp is happy to accept lists in place of tuples!
+# patches = diff.patch_make(oo, delta)
+patches = diff.patch_make(oo, delta)
+patch_txt = diff.patch_toText(patches)
+print("patch converted to text:", patch_txt)
+# JSON encode
+patch_json = json.dumps(patch_txt)
+print('JSON formatted patches:', patch_json)
+# decode from JSON
+patch_txt_ = json.loads(patch_json)
+print('patches decoded from JSON:', patch_txt_)
+# reconstruct patches object
+patches_ = diff.patch_fromText(patch_txt_)
+assert patch_txt == patch_txt_
+
+# compare size of diff vs text vs binary
+print("diff size (bytes):", sys.getsizeof(delta))
+print("patch size (bytes):", sys.getsizeof(patch_txt))
+print("binary patch size (bytes):", sys.getsizeof(patches_))
+
 ## changes the object in place
 # diff.diff_cleanupSemantic(delta)
 # print('delta_semantic:', delta)
@@ -31,34 +55,143 @@ print('delta decoded from JSON:', delta_)
 # html = diff.diff_prettyHtml(delta)
 # print('html:', html)
 
-# calculate patchest to turn original text via diff into new text (Method 3)
-# returns array of patch objects
-# note that dmp is happy to accept lists in place of tuples!
-# patches = diff.patch_make(oo, delta)
-patches = diff.patch_make(oo, delta_)
 # now apply patches to oo (and check for zero delta against nc) 
 # and also fuzzy patch ns
-nc_ = diff.patch_apply(patches, oo) # check against nc, identical?
-ns_ = diff.patch_apply(patches, ns) # check if good patch?
+# nc_ = diff.patch_apply(patches, oo) # check against nc, identical?
+nc_ = diff.patch_apply(patches_, oo) # check against nc, identical?
+# ns_ = diff.patch_apply(patches, ns) # check if good patch?
+ns_ = diff.patch_apply(patches_, ns) # check if good patch?
 
 # print('nc_:', nc_)
 # print('nc:', nc)
 assert nc_[0] == nc
-print()
-print('patches:', diff.patch_toText(patches))
-print('ns_:', ns_)
-
-
+# print()
+# print('patches:', diff.patch_toText(patches))
+# print('ns_:', ns_)
 print()
 
-obj = {
-          'clock':[-1,-1], 
-          'edits': [[0, 'Mac'], [1, 'intoshe'], \
-                    [0, 's had the original point and click '], \
-                    [-1, 'UI'], [1, 'interface'], [0, '.']],
-          'client_id': None
-        }
-encoded = json.dumps(obj)
-decoded = json.loads(encoded)
-print(encoded)
-print(decoded)
+# obj = {
+#           'clock':[-1,-1], 
+#           'edits': [[0, 'Mac'], [1, 'intoshe'], \
+#                     [0, 's had the original point and click '], \
+#                     [-1, 'UI'], [1, 'interface'], [0, '.']],
+#           'client_id': None
+#         }
+# encoded = json.dumps(obj)
+# decoded = json.loads(encoded)
+# print(encoded)
+# print(decoded)
+
+## Try longer text
+
+oo = """A Project Gutenberg of Australia eBook
+
+Title: Gone With The Wind
+Author: Margaret Mitchell (1900-1949)
+eBook No.:  0200161.txt
+Edition:    1
+Language:   English
+Character set encoding:     ASCII--7 bit
+Date first posted: February 2002
+Date most recently updated: November 2010
+
+This eBook was produced by: Don Lainson dlainson@sympatico.ca
+
+Project Gutenberg of Australia eBooks are created from printed editions
+which are in the public domain in Australia, unless a copyright notice
+is included. We do NOT keep any eBooks in compliance with a particular
+paper edition.
+
+Copyright laws are changing all over the world. Be sure to check the
+copyright laws for your country before downloading or redistributing this
+file.
+
+This eBook is made available at no cost and with almost no restrictions
+whatsoever. You may copy it, give it away or re-use it under the terms
+of the Project Gutenberg of Australia License which may be viewed online at
+http://gutenberg.net.au/licence.html
+
+To contact Project Gutenberg of Australia go to http://gutenberg.net.au
+
+
+Title:      Gone With The Wind
+Author:     Margaret Mitchell (1900-1949)
+
+
+
+
+PART ONE
+
+
+
+CHAPTER I
+
+
+Scarlett O'Hara was not beautiful, but men seldom realized it when
+caught by her charm as the Tarleton twins were.  In her face were
+too sharply blended the delicate features of her mother, a Coast
+aristocrat of French descent, and the heavy ones of her florid
+Irish father.  But it was an arresting face, pointed of chin,
+square of jaw.  Her eyes were pale green without a touch of hazel,
+starred with bristly black lashes and slightly tilted at the ends.
+Above them, her thick black brows slanted upward, cutting a
+startling oblique line in her magnolia-white skin--that skin so
+prized by Southern women and so carefully guarded with bonnets,
+veils and mittens against hot Georgia suns.
+
+Seated with Stuart and Brent Tarleton in the cool shade of the
+porch of Tara, her father's plantation, that bright April
+afternoon of 1861, she made a pretty picture.  Her new green
+flowered-muslin dress spread its twelve yards of billowing
+material over her hoops and exactly matched the flat-heeled green
+morocco slippers her father had recently brought her from Atlanta.
+The dress set off to perfection the seventeen-inch waist, the
+smallest in three counties, and the tightly fitting basque showed
+breasts well matured for her sixteen years.  But for all the
+modesty of her spreading skirts, the demureness of hair netted
+smoothly into a chignon and the quietness of small white hands
+folded in her lap, her true self was poorly concealed.  The green
+eyes in the carefully sweet face were turbulent, willful, lusty
+with life, distinctly at variance with her decorous demeanor.
+Her manners had been imposed upon her by her mother's gentle
+admonitions and the sterner discipline of her mammy; her eyes were
+her own."""
+nc = oo + " asdf"
+## Looks like best to transmit patch in general, except for whole-document
+## transfer cases (in the event of initial handshake, error recovery)
+# nc = " asdf"
+
+# delta is the currency of exchange between client and server
+delta = diff.diff_main(oo, nc)
+patches = diff.patch_make(oo, delta)
+patch_txt = diff.patch_toText(patches)
+# reconstruct patches object
+patches_ = diff.patch_fromText(patch_txt_)
+
+# compare size of diff vs text vs binary
+print("diff size (bytes):", sys.getsizeof(delta))
+print("patch size (bytes):", sys.getsizeof(patch_txt))
+print("binary patch size (bytes):", sys.getsizeof(patches_))
+
+# print ("delta:", delta)
+# print("patch text:", patch_txt)
+
+
+# what happens if transfering altogether new text:
+oo = ''
+nc = "Altogether brand new doc. See how it goes.\n or doesn't."
+delta = diff.diff_main(oo, nc)
+patches = diff.patch_make(oo, delta)
+new_doc = diff.patch_apply(patches, "") # check that it's not rejected
+print()
+print("new doc:", new_doc)
+print('delta:', delta)
+print(diff.patch_toText(patches))
+
+
+print('now apply patch to smth completely different:', diff.patch_apply(patches, "Something completely different"))
+
+new_doc = diff.patch_apply(diff.patch_fromText(''), "asdf") # what if empty patch string??
+print("empty patch applied to asdf:", new_doc) # looks like a bug?
+
+print(diff.patch_toText(diff.patch_make('',diff.diff_main('', ''))))

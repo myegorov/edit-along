5db0e00d32eb63a51280e4f54c7a94b063418319
Maksim Yegorov <findmaksim@gmail.com>
2017-05-10 22:44:56 -0400

    fixed cursor; todo: error scenarios

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 window.onload = function() {
-  // var SYNC_INTERVAL = 1000; // diff every 1 sec?
-  var SYNC_INTERVAL = 500; // diff every 0.5 sec?
+  var SYNC_INTERVAL = 500; // diff every 1 sec?
   var CURSOR = '¶';
 
   var dmp_exact = new diff_match_patch(0.0);
@@ -92,7 +91,8 @@ window.onload = function() {
     if (client.awaitResponse) return;
     // if (! tab.text.updated) return; // we're also interested in any server updates
     tab.text.updated = false;
-    tab.text.str = textarea.innerText;
+    // tab.text.str = textarea.innerText;
+    tab.text.str = textarea.value;
 
     /* diff Client Text (new) against Client Shadow (old) */
     var delta = dmp_exact.diff_main(tab.shadow.str, tab.text.str, false);
@@ -145,95 +145,31 @@ window.onload = function() {
       tab.shadow.clock[1] += 1;
 
       /* apply fuzzy patch to Client Text */
-      // TODO: working here below on cursors...
-      console.log('about to tweak cursor because of edits: ' + msg.edits);
-      var cursorPos = getCaretCharacterOffsetWithin(textarea); // int index
-      console.log('old cursor was at: ' + cursorPos);
-      var textSnapshot = textarea.innerText;
+      // get cursor position
+      textarea.focus();
+      var cursorPos = textarea.selectionStart;
+
+      var textSnapshot = textarea.value;
       var textWithCursor = textSnapshot.slice(0,cursorPos) + CURSOR +
                             textSnapshot.slice(cursorPos);
       var updatedText = dmp_fuzzy.patch_apply(
-                            // dmp_fuzzy.patch_fromText(msg.edits), tab.text.str)[0]
                             dmp_fuzzy.patch_fromText(msg.edits), textWithCursor)[0]
       // now extract cursor char & reset cursor
       var newCursorPos = updatedText.indexOf(CURSOR);
-      console.log('found cursor char at: ' + newCursorPos);
-      if (newCursorPos < 0) {newCursorPos = 
-                                Math.min(cursorPos, updatedText.length-1);
-      }
-      else {
+      if (newCursorPos < 0) {
+        newCursorPos = 0; // revert to start of text
+      } else { // excise cursor
         updatedText = updatedText.slice(0,newCursorPos) + 
                                 updatedText.slice(newCursorPos+1);
-        newCursorPos = Math.min(newCursorPos, updatedText.length-1);
       }
-      console.log('will set new cursor at: '+newCursorPos);
 
-
-      console.log('applied fuzzy patch to client text: ' + updatedText);
       tab.text.str = updatedText;
-      textarea.innerText = updatedText;
-      setCaretAt(textarea, newCursorPos);
+      textarea.value= updatedText;
+
+      // update cursor
+      textarea.focus();
+      textarea.setSelectionRange(newCursorPos, newCursorPos);
     }
     client.awaitResponse = false;
   }
-
-
-  // utils for working with cursor
-  function getCaretCharacterOffsetWithin(element) {
-    var caretOffset = 0;
-    if (typeof window.getSelection != "undefined") {
-      var range = window.getSelection().getRangeAt(0);
-      var preCaretRange = range.cloneRange();
-      preCaretRange.selectNodeContents(element);
-      preCaretRange.setEnd(range.endContainer, range.endOffset);
-      caretOffset = preCaretRange.toString().length;
-    }
-    return caretOffset;
-  };
-
-  function setCaretAt(element, ix) {
-    element.focus();
-    var range = document.createRange();
-    var sel = window.getSelection();
-    var relIx = 0;
-    var nodeIx = 0;
-    var found = false;
-    var ln = 0;
-    // alert(element.innerHTML);
-    for (var i = 0; i < element.childNodes.length; i++) {
-      // alert(element.childNodes[i].nodeType);
-      if (element.childNodes[i].nodeType == 3) { // text node
-        ln = element.childNodes[i].length;
-      } else { // <br>
-        // ln = element.childNodes[i].innerText.length;
-        ln = 0;
-      }
-      if (ix >= ln) {
-        ix -= ln;
-        nodeIx += 1;
-      } else {
-        // nodeIx = i;
-        relIx = ix;
-        found = true;
-        break;
-      }
-      console.log('node: ' + nodeIx + ' relIx: ' + relIx + ' ix: ' + ix);
-    }
-    if (found) {
-      // // alert("found loc at node " + nodeIx + " relIx " + relIx);
-      // if (element.childNodes[nodeIx].nodeType == 3) {
-      //   range.setStart(element.childNodes[nodeIx], relIx);
-      //   range.setEnd(element.childNodes[nodeIx], relIx+1);
-      // } else {
-      //   range.setStart(element.childNodes[nodeIx].firstChild, relIx);
-      //   range.setEnd(element.childNodes[nodeIx].firstChild, relIx+1);
-      // }
-      range.setStart(element.childNodes[nodeIx], relIx);
-      range.collapse(true);
-      sel.removeAllRanges();
-      sel.addRange(range);
-      console.log('set new cursor at: ' + relIx);
-    }
-  };
-
 }
